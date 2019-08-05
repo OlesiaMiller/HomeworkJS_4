6242d409ccc4129ed108b268100f063c47c99702
Olesia Miller <olesyaenstein@gmail.com>
2019-08-05 21:59:30 +0300

    закончила домашнее задание

--- main.js
+++ main.js
@@ -30,50 +30,52 @@ console.log(wordsList(myLongStr, 'lor'));
 
 // 2
 
-// function getLocalDate(date, isSeconds, isISO) {
-//   var myDate = new Date();
-//   return myDate;
-// }
 
-/*
- * #2
- *
- * Создайте функцию getLocalDate(date, isSeconds, isISO), которая будет принимать любую
- * дату от конструктора new Date и преобразовывать ее в следующие форматы в зависимости от параметров:
- * getLocalDate(date) → dd.mm.yyyy, hh:mm, например: 16.07.2019, 00:15
- * getLocalDate(date, true) → dd.mm.yyyy, hh:mm, например: 16.07.2019, 00:15:32
- * getLocalDate(date, false, true) → dd.mm.yyyy, hh:mm, например: 2019-06-02, 00:15
- * getLocalDate(date, true, true) → dd.mm.yyyy, hh:mm, например: 2019-06-02, 00:15:32
- * date – любая дата из конструктора new Date().
- * isSeconds – опциональный параметр для отображения секунд в дате.
- * isISO – опциональный параметр переключения формата даты.
- */
+var myDate = new Date();
+function getLocalDate(date, isSeconds = false, isISO = false) {
+  var reg = new RegExp(':\\d{2}$', 'gui');
+  var res;
 
-// let myDate = new Date();
+  if (!isISO) res = isSeconds
+  ? date.toLocaleString()
+  : date.toLocaleString().replace(reg, '');
+else {
+  var year = date.getFullYear();
+  var month =
+    date.getMonth() + 1 < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  var day = date.getDate() < 9 ? `0${date.getDate()}` : date.getDate();
+  var hour = date.getHours() < 9 ? `0${date.getHours()}` : date.getHours();
+  var minutes =
+    date.getMinutes() < 9 ? `0${date.getMinutes()}` : date.getMinutes();
+  var seconds =
+    date.getSeconds() < 9 ? `0${date.getSeconds()}` : date.getSeconds();
 
-// console.log(getLocalDate(myDate)); // 16.07.2019, 00:15
+  res = isSeconds
+    ? `${year}-${month}-${day}, ${hour}:${minutes}:${seconds}`
+    : `${year}-${month}-${day}, ${hour}:${minutes}`;
+}
 
-// console.log(getLocalDate(myDate, true)); // 16.07.2019, 00:15:32
+return res;
+};
 
-// console.log(getLocalDate(myDate, false, true)); // 2019-06-02, 00:15
 
-// console.log(getLocalDate(myDate, true, true)); // 2019-06-02, 00:15:32
 
-// console.log(getLocalDate(new Date(123456)));
 
-// console.log(getLocalDate(new Date(123456), true));
-
-// console.log(getLocalDate(new Date(123456), false, true));
-
-// console.log(getLocalDate(new Date(123456), true, true));
+console.log(getLocalDate(myDate)); // 16.07.2019, 00:15
+console.log(getLocalDate(myDate, true)); // 16.07.2019, 00:15:32
+console.log(getLocalDate(myDate, false, true)); // 2019-06-02, 00:15
+console.log(getLocalDate(myDate, true, true)); // 2019-06-02, 00:15:32
+console.log(getLocalDate(new Date(123456)));
+console.log(getLocalDate(new Date(123456), true));
+console.log(getLocalDate(new Date(123456), false, true));
+console.log(getLocalDate(new Date(123456), true, true));
 
 // 3
 
 function getWeekDay(date) {
     var dat = new Date(date);
     var days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
-  
-    return days[dat.getDay()];
+      return days[dat.getDay()];
   }
   
 console.log(getWeekDay('2019-01-30')); // среда
@@ -91,14 +93,11 @@ function getLocalDay(date) {
 
 }
 
-
 console.log(getLocalDay('2019-07-16')); // 2
 console.log(getLocalDay('2019-07-25')); // 4
 console.log(getLocalDay('2019-07-27')); // 6
 console.log(getLocalDay('2019-08-02'));
 console.log(getLocalDay('2019-08-04'));
-
-
 
 // 5
 
@@ -122,36 +121,32 @@ console.log(getDateAgo('2019-01-29', 2)); // 27.01.2019
 console.log(getDateAgo('2019-01-29', 365)); // 29.01.2018
 
 // 6
-
-
-
 var yearNow = yearNow = new Date().getFullYear();
 
-function fullInfo() {
-  return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
-}
-var Car = {
-  constructor: function(engine, model, name, year) {
-    this.engine = engine;
-    this.model = model;
-    this.name = name;
-    this.year = year;
-    return this;
-  },
-  info: fullInfo,
-  get used() {
-    return yearNow - this.year > 1 ? 'used' : 'new';
+var Car = function(engine, model, name, year) {
+  this.engine = engine;
+  this.model = model;
+  this.name = name;
+  this.year = year;
+};
+Object.defineProperties(Car.prototype, {
+  used: {
+    get() {
+      return yearNow - this.year > 1 ? 'used' : 'new';
     },
-  set used(value) {
+
+  set (value) {
     if (value === 'new' && this.year < yearNow) this.year = yearNow;
     }
-  };
+  }
+});
 
-var car, car2;
 
-car = Object.create(Car).constructor(2000, 'Lacetti', 'Chevrolet', 2010);
-car2 = Object.create(Car).constructor(5000, 'FX50 AWD', 'Infinite', 2019);
-
+  Car.prototype.info = function() {
+    return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+  }
+  var car = new Car(2000, 'Lacetti', 'Chevrolet', 2010);
+  var car2 = new Car(5000, 'FX50 AWD', 'Infinite', 2019);
 
 console.log(car.info()); // chevrolet Lacetti, 2010cc, year 2010, used
 car.used = 'new';
@@ -162,28 +157,11 @@ console.log(car2.info()); // infinite FX50 AWD, 2019cc, year 2019, new
 car.used = 'used';
 console.log(car2.info()); // infinite FX50 AWD, 2019cc, year 2019, new -- изменения не выполняются
 
-/*
- * #6
- *
- * Используя в качестве основы, объект car, описанный в прошлом занятии, создайте прототип Car,
- * реализующий те же поля (#17.4) и методы(#17.5 и #17.6) у создаваемых объектов.
- *
- * Например:
- * let car = new Car(2000, 'Lacetti', 'Chevrolet', 2010);
- * let car2 = new Car(5000, 'FX50 AWD', 'Infinite', 2019);
- *
- * Способ создания прототипа не регламентирован.
- * Объекты и их методы, созданные прототипом должны полностью соответствовать объектам из прошлого задания.
- */
-
-
-
-
 // 7
 function testPerformance(iterations, func) {
     var d = Date.now();
     if (typeof func === 'function') 
-        for (var i = 0; i<= iterations; i++) func();
+        for (var i = 0; i<= iterations; ++i) func();
         return Date.now() - d;
 
 };
@@ -199,12 +177,13 @@ function test1() {
 }
 
 function test2() {
-  const reg = new RegExp('[oaeui]', 'gui');
+  var reg = new RegExp('[oaeui]', 'gui');
   myLongStr.replace(reg, '');
 }
-
 console.log(testPerformance(100, test1)); // time
 console.log(testPerformance(100, test2)); // time
 console.log(testPerformance(100, 12345)); // 0
+
+
 
 
